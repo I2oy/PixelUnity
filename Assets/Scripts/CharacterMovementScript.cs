e2dfaf1a98d61919beafacb57614121a9590d6e4
I2oy <i2oy.21@gmail.com>
2015-01-08 22:52:08 -0700

    Initial Project Start
    Includes initial work on animation and test sprite sheet.

---
+++ Assets/Scripts/CharacterMovementScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovementScript : MonoBehaviour {
+
+
+	Animator characterAnimator;
+	// Use this for initialization
+	void Start () {
+	
+		characterAnimator = gameObject.GetComponent<Animator>();
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		playerInputMovementAnimation();
+
+	}
+
+
+	void playerInputMovementAnimation()
+	{
+		if(Input.GetKey(KeyCode.D))
+		{
+			characterAnimator.SetBool("Running",true);
+
+		}
+		else
+		{
+			characterAnimator.SetBool("Running",false);
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			characterAnimator.SetBool("Jumping",true);
+			Invoke("stopJump",2f);
+		}
+
+	}
+
+	void stopJump()
+	{
+		characterAnimator.SetBool ("Jumping",false);
+
+	}
+}
