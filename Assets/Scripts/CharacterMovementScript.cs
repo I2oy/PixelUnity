5567de11299e85385d1675cb7b42a8f11c0b73f6
I2oy <i2oy.21@gmail.com>
2015-01-10 15:54:35 -0700

    Initial Movement Animations and controls
    Added a few more sprites and animations.

--- Assets/Scripts/CharacterMovementScript.cs
+++ Assets/Scripts/CharacterMovementScript.cs
@@ -3,48 +3,65 @@ using System.Collections;
 
 public class CharacterMovementScript : MonoBehaviour {
 
+	public float maxSpeed = 10f;
+	bool facingRight = true;
+	Animator anim;
 
-	Animator characterAnimator;
-	// Use this for initialization
-	void Start () {
-	
-		characterAnimator = gameObject.GetComponent<Animator>();
-
-
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-		playerInputMovementAnimation();
-
-	}
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 700f;
 
 
-	void playerInputMovementAnimation()
+
+	void Start()
 	{
-		if(Input.GetKey(KeyCode.D))
-		{
-			characterAnimator.SetBool("Running",true);
-
-		}
-		else
-		{
-			characterAnimator.SetBool("Running",false);
-		}
-
-		if(Input.GetKeyDown(KeyCode.Space))
-		{
-			characterAnimator.SetBool("Jumping",true);
-			Invoke("stopJump",2f);
-		}
-
+		anim = GetComponent<Animator> ();
 	}
 
-	void stopJump()
+	void Update()
 	{
-		characterAnimator.SetBool ("Jumping",false);
 
+		//Change to Button Jump Axis
+		if(grounded && Input.GetKeyDown(KeyCode.Space))
+		{
+			anim.SetBool("Ground", false);
+			rigidbody2D.AddForce(new Vector2(0, jumpForce));
+
+		}
+	}
+
+	void FixedUpdate()
+	{
+
+		grounded = Physics2D.OverlapCircle (groundCheck.position,groundRadius, whatIsGround);
+		anim.SetBool ("Ground",grounded);
+
+		anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
+
+
+
+		float move = Input.GetAxis ("Horizontal");
+
+		anim.SetFloat ("Speed",Mathf.Abs(move));
+
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+
+		if (move > 0 && !facingRight)
+			Flip ();
+		else if (move < 0 && facingRight)
+			Flip ();
+
+
+
+	}
+	void Flip()
+	{
+
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
 	}
 }
